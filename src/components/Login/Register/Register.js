82684ec1520a85abd047bf8d7ed238f079d11c7e
web-altaf-1 <web.altaf.1@gmail.com>
2022-05-17 23:06:56 +0600

    register page user crated successfull

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value ;
+        const password = event.target.password.value ;
+        console.log(name,email,password);
+        
+        createUserWithEmailAndPassword(email, password);
+
+    }
     return (
         <div className=''>
             <h2 className='text-center text-primary my-3'>Please Register</h2>
-            <Form className='w-50 mx-auto shadow-lg p-5 bg-white mb-5'>
+            <Form className='w-50 mx-auto shadow-lg p-5 bg-white mb-5' onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Your Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Your Name" autoComplete='off' required />
+                    <Form.Control type="text" placeholder="Enter Your Name" name='name' autoComplete='off' required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" autoComplete='off' required />
+                    <Form.Control type="email" placeholder="Enter email" name='email' autoComplete='off' required />
                 </Form.Group>
 
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" autoComplete='off' required />
+                    <Form.Control type="password" placeholder="Password" name='password' autoComplete='off' required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" required />
@@ -30,7 +50,7 @@ const Register = () => {
                 <br />
                 <Form.Label className='mt-2'>Already Have an Account ??
                 </Form.Label>
-                <Link style={{ marginLeft: '5px' }} to='/login'>Please Login</Link>
+                <Link  style={{ marginLeft: '5px' }} to='/login'>Please Login</Link>
                 <br />
             </Form>
         </div>
